8742487ecee30b405d76448e8d6850e3fc79f4aa
Timo <tr110@hdm-stuttgart.de>
2025-03-08 17:23:23 +0200

    Add variables directory

---
+++ variables/target/debug/deps/variables.d
@@ -0,0 +1,5 @@
+C:\Users\Timo\projects\rust\variables\target\debug\deps\variables.exe: src\main.rs
+
+C:\Users\Timo\projects\rust\variables\target\debug\deps\variables.d: src\main.rs
+
+src\main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+C:\Users\Timo\projects\rust\variables\target\debug\variables.exe: C:\Users\Timo\projects\rust\variables\src\main.rs

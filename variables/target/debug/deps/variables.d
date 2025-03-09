c15e319032000035111d97050f427bc42e2c37b8
timorischer <160324459+timorischer@users.noreply.github.com>
2025-03-09 22:25:06 +0200

    Delete variables/target directory

--- variables/target/debug/deps/variables.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Timo\projects\rust\variables\target\debug\deps\variables.exe: src\main.rs
-
-C:\Users\Timo\projects\rust\variables\target\debug\deps\variables.d: src\main.rs
-
-src\main.rs:

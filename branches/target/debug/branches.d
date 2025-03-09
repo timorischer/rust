956ae484912f96d56e65a67be1dd223d3e8bccb3
timorischer <160324459+timorischer@users.noreply.github.com>
2025-03-09 22:24:05 +0200

    Delete branches/target directory

--- branches/target/debug/branches.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Timo\projects\rust\branches\target\debug\branches.exe: C:\Users\Timo\projects\rust\branches\src\main.rs
--- branches/target/debug/deps/branches.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Timo\projects\rust\branches\target\debug\deps\branches.exe: src\main.rs
-
-C:\Users\Timo\projects\rust\branches\target\debug\deps\branches.d: src\main.rs
-
-src\main.rs:

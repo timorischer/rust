f72dd226983e23366fafe9d1714d6b6dba6cc496
Timo <tr110@hdm-stuttgart.de>
2025-03-09 19:48:07 +0200

    Add branches directory

---
+++ branches/target/debug/branches.d
@@ -0,0 +1,1 @@
+C:\Users\Timo\projects\rust\branches\target\debug\branches.exe: C:\Users\Timo\projects\rust\branches\src\main.rs
---
+++ branches/target/debug/deps/branches.d
@@ -0,0 +1,5 @@
+C:\Users\Timo\projects\rust\branches\target\debug\deps\branches.exe: src\main.rs
+
+C:\Users\Timo\projects\rust\branches\target\debug\deps\branches.d: src\main.rs
+
+src\main.rs:

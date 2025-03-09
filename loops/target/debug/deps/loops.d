a50492aee186530168255abcbd909fd238a2b0fc
Timo <tr110@hdm-stuttgart.de>
2025-03-09 20:11:04 +0200

    Add loops directory

---
+++ loops/target/debug/deps/loops.d
@@ -0,0 +1,5 @@
+C:\Users\Timo\projects\rust\loops\target\debug\deps\loops.exe: src\main.rs
+
+C:\Users\Timo\projects\rust\loops\target\debug\deps\loops.d: src\main.rs
+
+src\main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+C:\Users\Timo\projects\rust\loops\target\debug\loops.exe: C:\Users\Timo\projects\rust\loops\src\main.rs

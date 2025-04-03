7b7ba90a7c35139a70e770c3ccbdeaf8cc065ad8
Timo <tr110@hdm-stuttgart.de>
2025-04-03 16:32:08 +0200

    Add unit tests to rectangles/src/main.rs

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -36,3 +36,36 @@ fn main() {
     println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
     println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8, 
+            height: 7, 
+        };
+        let smaller = Rectangle {
+            width: 5, 
+            height: 1, 
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+}
